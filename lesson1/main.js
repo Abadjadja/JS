a3f9665112efab65847d12aa4557bbd2e186e0f1
Abadjadja <andreychubaty@gmail.com>
2022-02-01 01:25:23 +0300

    1 пункт домашки

---
+++ lesson1/main.js
@@ -0,0 +1,65 @@
+let msg = 'hello';
+console.log(msg);
+alert(msg);
+document.write(msg)
+msg = 'world'
+
+let title = 'owu';
+console.log(title);
+alert(title);
+document.write(title)
+
+let domeOne = 'com';
+console.log(domeOne);
+alert(domeOne);
+document.write(domeOne)
+
+let domenTwo = 'ua';
+console.log(domenTwo);
+alert(domenTwo);
+document.write(domenTwo)
+
+let firstNumb = 1;
+console.log(firstNumb);
+alert(firstNumb);
+document.write(firstNumb)
+
+let secondNumb = 10;
+console.log(secondNumb);
+alert(secondNumb);
+document.write(secondNumb)
+
+let thirdNumb = -999;
+console.log(thirdNumb);
+alert(thirdNumb);
+document.write(thirdNumb)
+
+let fourthNumb = 123;
+console.log(fourthNumb);
+alert(fourthNumb);
+document.write(fourthNumb)
+
+let pi = 3.14;
+console.log(pi)
+alert(pi);
+document.write(pi)
+
+let fifthNumb = 2.7;
+console.log(fifthNumb);
+alert(fifthNumb);
+document.write(fifthNumb)
+
+let sixthNumb = 16;
+console.log(sixthNumb);
+alert(sixthNumb);
+document.write(sixthNumb)
+
+let booly1 = 3=='3'
+console.log(booly1);
+alert(booly1);
+document.write(booly1)
+
+let booly2 = 3==='3'
+console.log(booly2);
+alert(booly2);
+document.write(booly2)
